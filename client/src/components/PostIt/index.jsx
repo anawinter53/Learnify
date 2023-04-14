10ff40556e1d8ad42cb01ee2478ec2bd16a047cd
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-14 22:17:34 +0100

    create PostIt component and add styling

--- client/src/components/PostIt/index.jsx
+++ client/src/components/PostIt/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import styles from "./index.module.css";
 
 export default function PostIt() {
   return (
     <div className={styles["card-container"]}>
-      <div className={styles["card-0"]}></div>
-      <div className={styles["card-1"]}></div>
-      <div className={styles["card-2"]}></div>
-      <div className={styles["card-3"]}></div>
+      <div className={`${styles["card"]} ${styles["card-0"]}`}>
+        <div className={styles["card-heading"]}></div>
+      </div>
+      <div className={`${styles["card"]} ${styles["card-1"]}`}>
+        <div className={styles["card-heading"]}></div>
+      </div>
+      <div className={`${styles["card"]} ${styles["card-2"]}`}>
+        <div className={styles["card-heading"]}></div>
+      </div>
+      <div className={`${styles["card"]} ${styles["card-3"]}`}>
+        <div className={styles["card-heading"]}></div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./index.module.css"
+import { PostIt } from "../../components"
 
 export default function Landing() {
   return (
     <div className={styles["landing"]}>
       <div className={styles["container"]}>
-        
+        <PostIt />
       </div>
     </div>
   )
