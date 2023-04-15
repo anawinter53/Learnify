d79cb6c30c7361181cc366ea5f238c51a0a46f6d
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-15 17:53:46 +0100

    add slide in navbar (code is very janky, will attempt to fix later)

--- client/src/layouts/LandingNav/index.jsx
+++ client/src/layouts/LandingNav/index.jsx
@@ -1,12 +1,57 @@
+import { useState, useEffect, useRef } from "react";
 import styles from "./index.module.css";
 import { Outlet, NavLink, useLocation } from "react-router-dom";
 
 export default function LandingNav() {
   const location = useLocation();
+  const [showNav, setShowNav] = useState(false);
+  const navRef = useRef();
+  
+  const toggleVisible = () => {
+    const { scrollTop } = document.documentElement;
+    if (scrollTop >= 100){
+      setShowNav(true);
+    } 
+    else if (scrollTop <= 100){
+      setShowNav(false)
+    }
+  };
+
+  window.addEventListener('scroll', toggleVisible);
 
   return (
     <>
-      <nav>
+      <nav
+        ref={navRef}
+        style={{ position: "relative", background: "transparent" }}
+      >
+        <div>
+          <NavLink to="/" className={styles["title"]}>
+            App Name
+          </NavLink>
+        </div>
+        <div>
+          {location.pathname === "/login" ? (
+            <NavLink
+              className={`${styles["signup"]} ${styles["btn"]}`}
+              to="/signup"
+            >
+              Sign up
+            </NavLink>
+          ) : (
+            <NavLink
+              className={`${styles["login"]} ${styles["btn"]}`}
+              to="/login"
+            >
+              Login
+            </NavLink>
+          )}
+        </div>
+      </nav>
+      <nav
+        ref={navRef}
+        style={{ top: showNav ? "0" : "-100px", transition: "top 0.3s" }}
+      >
         <div>
           <NavLink to="/" className={styles["title"]}>
             App Name
@@ -20,12 +65,23 @@ export default function LandingNav() {
         </div>
         <div>
           {location.pathname === "/login" ? (
-            <NavLink className={`${styles["signup"]} ${styles["btn"]}`} to="/signup">Sign up</NavLink>
+            <NavLink
+              className={`${styles["signup"]} ${styles["btn"]}`}
+              to="/signup"
+            >
+              Sign up
+            </NavLink>
           ) : (
-            <NavLink className={`${styles["login"]} ${styles["btn"]}`} to="/login">Login</NavLink>
+            <NavLink
+              className={`${styles["login"]} ${styles["btn"]}`}
+              to="/login"
+            >
+              Login
+            </NavLink>
           )}
         </div>
       </nav>
+
       <Outlet />
     </>
   );
--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,6 +1,6 @@
-import styles from "./index.module.css"
-import { PostIt, PostItSummary } from "../../components"
-import { NavLink } from "react-router-dom"
+import styles from "./index.module.css";
+import { PostIt, PostItSummary } from "../../components";
+import { NavLink } from "react-router-dom";
 
 export default function Landing() {
   return (
@@ -8,14 +8,18 @@ export default function Landing() {
       <div className={styles["container"]}>
         <PostIt />
         <div className={styles["info"]}>
-          <h1 className={styles["slogan"]}>Join our community of learners today and unlock your full potential.</h1>
-          <NavLink to="/signup" className={styles["btn"]}>Get Started!</NavLink>
+          <h1 className={styles["slogan"]}>
+            Join our community of learners today and unlock your full potential.
+          </h1>
+          <NavLink to="/signup" className={styles["btn"]}>
+            Get Started!
+          </NavLink>
         </div>
       </div>
-      <PostItSummary colour="orange" reverse={false}/>
-      <PostItSummary colour="red" reverse={true}/>
-      <PostItSummary colour="green" reverse={false}/>
-      <PostItSummary colour="blue" reverse={true}/>
+      <PostItSummary colour="orange" reverse={false} />
+      <PostItSummary colour="red" reverse={true} />
+      <PostItSummary colour="green" reverse={false} />
+      <PostItSummary colour="blue" reverse={true} />
     </div>
-  )
+  );
 }
