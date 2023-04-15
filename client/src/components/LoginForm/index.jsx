5c90b29ffa9a567888f008b059d67c39050203de
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-15 15:04:19 +0100

    add more styling to login page and fix aligment on landing page

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,11 +1,27 @@
-import styles from "./index.module.css"
+import { NavLink } from "react-router-dom";
+import styles from "./index.module.css";
 
 export default function LoginForm() {
   return (
     <div className={styles["form"]}>
       <div className={styles["form-heading"]}>
-        <h2>Login</h2>
+        <h1>Login</h1>
       </div>
+      <form>
+        <div className={styles["input"]}>
+          <input type="text" id="username" required />
+          <span>Username</span>
+        </div>
+        <div className={styles["input"]}>
+          <input type="password" id="password" required />
+          <span>Password</span>
+        </div>
+        <div className={styles["input"]}>
+          <button className={`${styles["btn"]} ${styles["btn-x"]}`} type="submit">Submit</button>
+        </div>
+        <p>Forgot Password</p>
+        <NavLink className={styles["redirect-signup"]} to="/signup">Don't have an account yet? Sign Up</NavLink>
+      </form>
     </div>
-  )
+  );
 }
--- client/src/layouts/LandingNav/index.jsx
+++ client/src/layouts/LandingNav/index.jsx
@@ -5,13 +5,18 @@ export default function LandingNav() {
   return (
     <>
       <nav>
-        <NavLink to="/" className={styles["title"]}>App Name</NavLink>
+        <div>
+          <NavLink to="/" className={styles["title"]}>App Name</NavLink>
+        </div>
         <div className={styles["options"]}>
           <div className={styles["btn"]}>Button 1</div>
           <div className={styles["btn"]}>Button 2</div>
           <div className={styles["btn"]}>Button 3</div>
+          <div className={styles["btn"]}>Button 4</div>
         </div>
-        <NavLink to="/login" className={`${styles["login"]} ${styles["btn"]}`}>Login</NavLink>
+        <div>
+          <NavLink to="/login" className={`${styles["login"]} ${styles["btn"]}`}>Login</NavLink>
+        </div>
       </nav>
       <Outlet />
     </>
