c8730e76765573d9a37007537eba7049383b6e92
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-15 16:27:27 +0100

    add dynamic landingnav navlink based on page

--- client/src/layouts/LandingNav/index.jsx
+++ client/src/layouts/LandingNav/index.jsx
@@ -1,12 +1,16 @@
-import styles from "./index.module.css"
-import { Outlet, NavLink } from "react-router-dom"
+import styles from "./index.module.css";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
 
 export default function LandingNav() {
+  const location = useLocation();
+
   return (
     <>
       <nav>
         <div>
-          <NavLink to="/" className={styles["title"]}>App Name</NavLink>
+          <NavLink to="/" className={styles["title"]}>
+            App Name
+          </NavLink>
         </div>
         <div className={styles["options"]}>
           <div className={styles["btn"]}>Button 1</div>
@@ -15,10 +19,14 @@ export default function LandingNav() {
           <div className={styles["btn"]}>Button 4</div>
         </div>
         <div>
-          <NavLink to="/login" className={`${styles["login"]} ${styles["btn"]}`}>Login</NavLink>
+          {location.pathname === "/login" ? (
+            <NavLink className={`${styles["signup"]} ${styles["btn"]}`} to="/signup">Sign up</NavLink>
+          ) : (
+            <NavLink className={`${styles["login"]} ${styles["btn"]}`} to="/login">Login</NavLink>
+          )}
         </div>
       </nav>
       <Outlet />
     </>
-  )
+  );
 }
