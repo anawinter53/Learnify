292c897059a637a1051a02c7f27b9fd799319ec9
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-16 19:56:59 +0100

    refactor test

--- client/src/components/PostIt/PostIt.test.jsx
+++ client/src/components/PostIt/PostIt.test.jsx
@@ -6,29 +6,39 @@ import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import PostIt from ".";
 import PostItSummary from "../PostItSummary";
 
-describe("PostIt Component", () => {
-  let card0Ref, card1Ref, card2Ref, card3Ref;
+const renderPostIt = () => {
+  const cardRefs = [
+    React.createRef(),
+    React.createRef(),
+    React.createRef(),
+    React.createRef(),
+  ];
 
-  beforeEach(() => {
-    card0Ref = React.createRef();
-    card1Ref = React.createRef();
-    card2Ref = React.createRef();
-    card3Ref = React.createRef();
-
-    render(
-      <BrowserRouter>
-        <PostIt
-          card0={card0Ref}
-          card1={card1Ref}
-          card2={card2Ref}
-          card3={card3Ref}
+  const { container } = render(
+    <BrowserRouter>
+      <PostIt
+        card0={cardRefs[0]}
+        card1={cardRefs[1]}
+        card2={cardRefs[2]}
+        card3={cardRefs[3]}
+      />
+      {cardRefs.map((ref, index) => (
+        <PostItSummary
+          key={index}
+          ref={ref}
+          colour={index % 2 ? "red" : "orange"}
+          reverse={index % 2 !== 0}
         />
-        <PostItSummary ref={card0Ref} colour="orange" reverse={false} />
-        <PostItSummary ref={card1Ref} colour="red" reverse={true} />
-        <PostItSummary ref={card2Ref} colour="green" reverse={false} />
-        <PostItSummary ref={card3Ref} colour="blue" reverse={true} />
-      </BrowserRouter>
-    );
+      ))}
+    </BrowserRouter>
+  );
+
+  return { container, cardRefs };
+};
+
+describe("PostIt Component", () => {
+  beforeEach(() => {
+    renderPostIt();
   });
 
   afterEach(() => {
