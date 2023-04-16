631cca1054bf922e14b738f45ad8de4399d25617
anawinter53 <anatolia.winter@yahoo.com>
2023-04-16 18:01:14 +0100

    add login test pass

---
+++ client/src/components/LoginForm/LoginForm.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import { screen, render, cleanup } from '@testing-library/react';
+
+import matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers);
+
+import LoginForm from '.';
+
+describe("LoginForm Component", () => {
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <LoginForm />
+            </BrowserRouter>
+        )
+    })
+    
+    afterEach(() => {
+        cleanup();
+    })
+
+    it("Displays a form", () => {
+        const form = screen.getByRole('form')
+
+        expect(form).toBeInTheDocument();
+    })
+
+})
