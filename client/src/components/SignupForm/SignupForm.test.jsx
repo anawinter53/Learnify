8b21c0166a6561d9c8e7f227ae5a0be3ad90e5ee
anawinter53 <anatolia.winter@yahoo.com>
2023-04-16 17:41:06 +0100

    add embedded nav test pass

--- client/src/components/SignupForm/SignupForm.test.jsx
+++ client/src/components/SignupForm/SignupForm.test.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
 import { screen, render, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import matchers from '@testing-library/jest-dom/matchers'
 expect.extend(matchers);
 
 import SignupForm from '.';
 
-describe("SignupForm component", () => {
+describe("SignupForm Component", () => {
     beforeEach(() => {
         render(
             <BrowserRouter>
