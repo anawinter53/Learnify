59bc5998c9f6a633debcc5224543852f7ad759bb
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-17 12:04:44 +0100

    style friends page

--- client/src/pages/Friends/index.jsx
+++ client/src/pages/Friends/index.jsx
@@ -1,5 +1,47 @@
+import styles from "./index.module.css";
+
 export default function Friends() {
   return (
-    <div>Friends</div>
-  )
+    <div className={styles["friends"]}>
+      <div className={styles["container"]}>
+        <h1 className={styles["title"]}>Friends</h1>
+        <div className={styles["inner-container"]}>
+          <div className={styles["content"]}>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+            <div className={styles["quiz-card"]}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles["leaderboard"]}>
+        <h1 className={styles["leaderboard-title"]}>Leaderboard</h1>
+        <div className={styles["top3-container"]}>
+          <div className={styles["top2"]}></div>
+          <div className={styles["top1"]}></div>
+          <div className={styles["top3"]}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
