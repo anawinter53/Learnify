347447cdb89f5ce70770d7645dd7eeed1a66d930
Rosie <rosie@greyhoundcs.com>
2023-04-17 15:02:52 +0100

    minor edits

--- client/src/layouts/SideNav/index.jsx
+++ client/src/layouts/SideNav/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import styles from "./index.module.css";
 
--- client/src/pages/Quizzes/index.jsx
+++ client/src/pages/Quizzes/index.jsx
@@ -2,6 +2,7 @@ export default function Quizzes() {
   return (
     <div>
       <h1>Quizzes</h1>
+      <p>Hello?</p>
     </div>
   )
 }
