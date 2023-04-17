dd53a54f03e27564b0f193b7484c88167a86c947
Rosie <rosie@greyhoundcs.com>
2023-04-17 12:06:40 +0100

    created user controller

--- api/models/User.js
+++ api/models/User.js
@@ -18,6 +18,14 @@ class User {
         return new User(response.rows[0])
     }
 
+    static async getOneByUsername(username) {
+        const response = await db.query("SELECT * FROM users WHERE username = $1;", [username]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate user.")
+        }
+        return new User(response.rows[0])
+    }
+
     static async checkIfAdmin(id) {
         const response =await db.query("SELECT isAdmin FROM users WHERE user_id = $1;", [id])
         if (response.rows.length != 1) {
@@ -25,6 +33,22 @@ class User {
         }
 
         return response.rows[0].isAdmin;
+    }
+
+    static async create(data) {
+        const { username, email, password } = data;
+        const response = await db.query("INSERT INTO user_account (username, email, password) VALUES ($1, $2, $3) RETURNING user_id;", [username, email, password])
+        const newId = response.rows[0].user_id;
+        const newUser = await User.getOneById(newId);
+        return newUser;
+    }
+
+    async destroy() {
+        const response = await db.query("DELETE FROM users WHERE username = $1;", [this.username])
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate user")
+        }
+        return response
     }
 }
 
