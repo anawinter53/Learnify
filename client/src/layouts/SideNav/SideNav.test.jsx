1864d1ff26b7e4d8d7e6ef2034d81724f333283a
anawinter53 <anatolia.winter@yahoo.com>
2023-04-17 11:32:58 +0100

    add another test pass

--- client/src/layouts/SideNav/SideNav.test.jsx
+++ client/src/layouts/SideNav/SideNav.test.jsx
@@ -7,13 +7,13 @@ import userEvent from '@testing-library/user-event'
 import matchers from '@testing-library/jest-dom/matchers'
 expect.extend(matchers)
 
-import LandingNav from '.'
+import SideNav from '.'
 
-describe('LandingNav Component', () => {
+describe('SideNav Component', () => {
     beforeEach(() => {
         render(
             <BrowserRouter>
-                <LandingNav />
+                <SideNav />
             </BrowserRouter>
         )
     })
@@ -22,15 +22,19 @@ describe('LandingNav Component', () => {
         cleanup();
     })
 
-    it("Displays a nav bar with 2 links", () => {
+    it("Displays a nav bar with 2 main links and 4 sublinks", () => {
         const nav = screen.getByRole('navigation')
         expect(nav).toBeInTheDocument();
         expect(nav.childNodes.length).toBe(2)
+        const options = screen.getByRole('options')
+        expect(options).toBeInTheDocument();
+        expect(options.childNodes.length).toBe(4)
     })
 
-    
-
-    // it('Shows nav when scrolling', async () => {
-    //     window.scrollTo = 110
-    // })
+    it('Changes location when a navlink is clicked', async () => {
+        expect(window.location.href).not.toContain('/quizzes');
+        const quizzes = screen.getByText('Quizzes');
+        await userEvent.click(quizzes)
+        expect(window.location.href).toContain('/quizzes');
+    })
 })
