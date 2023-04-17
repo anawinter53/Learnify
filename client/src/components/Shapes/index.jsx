b122ca8c616b24bcad4ac4dcfe7baeb93bec9b6d
anawinter53 <anatolia.winter@yahoo.com>
2023-04-17 09:05:04 +0100

    add shapes test pass

--- client/src/components/Shapes/index.jsx
+++ client/src/components/Shapes/index.jsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css"
 export default function Shapes() {
   return (
     <div className={styles["box"]}>
-      <div></div>
+      <div role='card'></div>
       <div></div>
       <div></div>
       <div></div>
