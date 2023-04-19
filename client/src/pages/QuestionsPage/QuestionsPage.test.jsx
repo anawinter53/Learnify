0ed7662946315e07c9744a015b703e47182cedfb
anawinter53 <anatolia.winter@yahoo.com>
2023-04-19 14:38:52 +0100

    add questions page test pass

--- client/src/pages/QuestionsPage/QuestionsPage.test.jsx
+++ client/src/pages/QuestionsPage/QuestionsPage.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
-import { screen, render, cleanup } from '@testing-library/react';
+import { screen, render, cleanup, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 
 import matchers from '@testing-library/jest-dom/matchers'
@@ -23,10 +23,10 @@ describe("Questions page", () => {
         cleanup();
     })
 
-    it("Displays a heading", () => {
-        const heading = screen.getByRole('heading')
-        expect(heading).toBeInTheDocument();
-        expect(heading.textContent).toBe("404: Page not found")
+    it("Displays a heading", async () => {
+        const heading =  screen.getByRole('heading')
+        waitFor(() => expect(heading).toBeInTheDocument());
+        waitFor(() => expect(heading.textContent).toBe("404: Page not found"))
     })
 
 })
