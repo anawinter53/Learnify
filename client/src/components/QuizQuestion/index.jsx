aec2982c81c2180769b6ae73a8898f3127589705
Rosie <rosie@greyhoundcs.com>
2023-04-19 09:09:36 +0100

    working on quiz functionality

--- client/src/components/QuizQuestion/index.jsx
+++ client/src/components/QuizQuestion/index.jsx
@@ -1,34 +1,58 @@
 import styles from './index.module.css'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 export default function QuizQuestion({questions}) {
     const [answers, setAnswers] = useState([])
     const [question, setQuestion] = useState([])
     const [count, setCount] = useState(0)
+    const [toggle, setToggle] = useState(false)
+    const optionsRef = useRef()
+
 
 
     const updateQuestion = () => {
-        setQuestion(questions[count])
+        if (count < questions.length) {
+            setQuestion(questions[count])
         setAnswers([questions[count].answer, questions[count].fake_answer1, questions[count].fake_answer2, questions[count].fake_answer3].sort(() => Math.random() - 0.5));
         setCount(count + 1)
+        setToggle(false)
+        for (const o of optionsRef.current.children) {
+            o.style.background = "white"
+        }
+        } else {
+            alert("Quiz finished")
+        }
+        
     }
 
     useEffect(() => {
         updateQuestion()
-        console.log(question)
     }, [])
 
     const handleCheck = (e) => {
-        console.log(e.target.className)
-        console.log(count)
-        updateQuestion()
+        setToggle(true)
+        const options = optionsRef.current.children
+        for (const o of options) {
+            console.log(o.className)
+            if (o.className == "_correct_1inp1_21 undefined") {
+                o.style.background = "green"
+            } else {
+                o.style.background = "red"
+            }
+        }
+        
+        console.log(options)
+        const timer = setTimeout(() => {
+            updateQuestion()
+        }, 2000)
+        
     }
 
     return(
         <div className={styles["container"]}>
             <h2>{question.question}</h2>
-            <div className={styles['options-container']}>{answers.map((answer) => 
-                <button className={styles[answer === question.answer ? 'correct' : 'incorrect']} onClick={handleCheck}>{answer}</button>
+            <div className={styles['options-container']} ref={optionsRef}>{answers.map((answer) => 
+                <button className={`${styles[answer === question.answer ? 'correct' : 'incorrect']} ${styles[toggle ? 'answered' : '']}`}  onClick={handleCheck}>{answer}</button>
             )}</div>
         </div>
     )
