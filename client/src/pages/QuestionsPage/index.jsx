f2ca4b93766d5311ff5936080a54fd3c8283b3d8
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-20 22:48:12 +0100

    did major styling to questions page and refactored code (hopefully i didnt break anything, i am truly sorry if i did it is very late and im just typing this message and i dont when to stop please send help thx)

--- client/src/pages/QuestionsPage/index.jsx
+++ client/src/pages/QuestionsPage/index.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { QuizQuestion } from "../../components";
 import styles from "./index.module.css"
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function QuestionsPage() {
   const [loading, setLoading] = useState(true)
   const [questions, setQuestions] = useState([])
+  const [incorrect, setIncorrect] = useState([])
   const { subject } = useParams()
+  const [isFinished, setIsFinished] = useState(false)
+  const [score, setScore] = useState(0)
+
+  const navigate = useNavigate()
 
   async function getQuestions() {
     const category = subject ? subject.toLowerCase() : undefined
@@ -41,12 +46,33 @@ export default function QuestionsPage() {
 
   function displayQuestion() {
     return (
-      <div className={styles["quiz-questions"]}>
-        <div className={styles["container"]}>
-            <h1 className={styles["title"]}>Quizzes</h1>
-            <QuizQuestion questions={questions} updateScore={updateScore}/>
+      <>
+        <div style={{ display: isFinished ? "flex" : "none" }} className={`${styles["overlay"]}`}>
+          <div className={styles["popup"]}>
+            <div className={styles["message"]}>
+              <h2>Quiz Complete!</h2>
+              <p className={styles["score"]}>You scored {score} / {questions.length}</p>
+              <div className={styles["incorrect"]}>
+                <h2>Incorrect answers</h2>
+                {incorrect.map((inc, i) => {
+                  return (
+                    <p key={i} className={styles["incorrect-answer"]} >{inc.question} - <span className={styles["inc-answer"]}>{inc.answer}</span></p>
+                  )
+                })}
+              </div>
+            </div>
+            <div className={styles["options"]}>
+              <button onClick={() => navigate(-1)} className={styles["btn"]}>Return to Categories</button>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className={styles["quiz-questions"]}>
+          <div className={styles["container"]}>
+              <h1 className={styles["title"]}>Quizzes</h1>
+              <QuizQuestion questions={questions} updateScore={updateScore} setIsFinished={setIsFinished} score={score} setScore={setScore} incorrect={incorrect} setIncorrect={setIncorrect}/>
+          </div>
+        </div>
+      </>
     )
   }
 
